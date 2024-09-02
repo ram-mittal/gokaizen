434b835b148b9137364f812ff92abb1b95e711ca
Abin Justin Kumaravel <abinjustinkumaravel@gmail.com>
2024-09-02 20:13:40 +0000

    Lunch page

--- aysisphere/src/App.js
+++ aysisphere/src/App.js
@@ -1,14 +1,12 @@
-// import './App.css';
+import Logo from "./Assets/FULL-LOGO.png";
 
 function App() {
   return (
 
     <div className="flex items-center justify-center h-screen bg-black">
-      
-      <img className="" src="./" />
-      <h1 className="font-extrabold text-5xl text-white">
-        Comming Soon
-      </h1>
+      <img className="absolute top-12 w-52 left-24" src={Logo} alt="" />
+       <div className="w-[900px] h-[900px] bg-white fixed rounded-full -z-10"></div>
+      <h1 className="font-extrabold text-5xl text-white"> We are <span className="text-blue-700" >Launching</span> Soon</h1>
     </div>
   );
 }
