7c3923bdd37f07954dd86c5ce0e6078c37b33483
Abin Justin Kumaravel <abinjustinkumaravel@gmail.com>
2024-09-02 19:09:28 +0000

    first commit

---
+++ aysisphere/src/App.js
@@ -0,0 +1,16 @@
+// import './App.css';
+
+function App() {
+  return (
+
+    <div className="flex items-center justify-center h-screen bg-black">
+      
+      <img className="" src="./" />
+      <h1 className="font-extrabold text-5xl text-white">
+        Comming Soon
+      </h1>
+    </div>
+  );
+}
+
+export default App;
