47dfe3b0b9242fdd6bc6aecf3a66498538cf293a
Abin Justin Kumaravel <abinjustinkumaravel@gmail.com>
2024-09-09 22:22:07 +0000

    Socials little

--- aysisphere/src/components/socials.js
+++ aysisphere/src/components/socials.js
@@ -4,7 +4,7 @@ import { faFacebookF, faInstagram, faXTwitter, faLinkedinIn} from '@fortawesome/
 
 const socials = () => {
   return (
-    <div className='sm:w-[20%] sm:m-[2.5%] sm:static absolute bottom-[50px] left-[30%]'>
+    <div className='sm:w-[20%] sm:m-[2.5%] sm:static absolute sm:bottom-[50px] bottom-[20px] sm:left-[30%] left-[25%]'>
       <div className='flex sm:flex-col gap-10'>
         <FontAwesomeIcon color='white' icon={faFacebookF}/>
         <FontAwesomeIcon color="white" icon={faInstagram}/>
