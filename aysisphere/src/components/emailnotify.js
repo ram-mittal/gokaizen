8e0f0f4f61566e395ae1df92366044257707cffc
Abin Justin Kumaravel <abinjustinkumaravel@gmail.com>
2024-09-09 20:18:28 +0000

    made spell corrections

--- aysisphere/src/components/emailnotify.js
+++ aysisphere/src/components/emailnotify.js
@@ -5,7 +5,7 @@ function emailnotify() {
     <div>
       <div className='relative flex bottom-[200px] items-center justify-center text-center'>
         <form>
-          <p className="text-white tracking-[2px]"> Subscripe Us To Get Instant Notification</p>
+          <p className="text-white tracking-[2px]"> Subscribe Us To Get Instant Notification</p>
           <div className='flex w-fit p-2 m-4 sm:gap-[50px] gap-[20px] rounded-md'>          
             <input type="text" placeholder='Email' className='border-1 rounded-md bg-transparent text-white sm:w-[350px] w-[250px] text-[20px] p-2 focus:outline-none focus:border-none' >
             </input>
